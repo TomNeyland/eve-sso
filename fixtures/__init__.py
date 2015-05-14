c2ff052ddad36ca8433b18b89d6e45470dc02f7e
Tom Neyland <tcneyland+github@gmail.com>
2015-05-14 14:36:54 -0400

    fix(fixtures): Fixes bug caused by spacing issues

--- fixtures/__init__.py
+++ fixtures/__init__.py
@@ -2,9 +2,7 @@ from fixture import SQLAlchemyFixture, DataSet
 from evesso import db
 from evesso.models import Chatroom
 
-
 dbfixture = SQLAlchemyFixture(env={'ChatroomData': Chatroom}, engine=db.engine)
-
 
 class ChatroomData(DataSet):
 
@@ -12,6 +10,6 @@ class ChatroomData(DataSet):
         name = "notifications"
         motd = "This chatroom contains everyone"
 
-	class general:
-		name = "general"
-		motd = "gbs in here"
+    class general:
+        name = "general"
+        motd = "gbs in here"
